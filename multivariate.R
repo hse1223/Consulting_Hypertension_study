803f3bd609e8377330b800056baa2a9240b544f2
Anthony Li <38049831+doccstat@users.noreply.github.com>
2023-11-16 16:07:02 -0600

    categorical doc level

--- multivariate.R
+++ multivariate.R
@@ -1,9 +1,16 @@
 rm(list=ls())
 source("read_data.R")
 
-X_variables = DATA[,c(categorical[3], dummy[4], dummy[7])]
+X_Y = data.frame(
+    X1 = as.factor(DATA[,categorical[3]]),
+    X2 = DATA[,dummy[4]],
+    X3 = DATA[,dummy[7]],
+    Y = Y_variables
+)
 
-glm_res = glm(Y_variables~X_variables[,1] + X_variables[,2] + X_variables[,3])
+X_Y$X1 <- relevel(X_Y$X1, ref = "1")
+
+glm_res = glm(Y ~ X1 + X2 + X3, data = X_Y)
 
 summary(glm_res)
 
