eaf8b4dfc2fb0e701f9a90000fa23decd61398c9
hse1223 <a01027118607@gmail.com>
2023-11-17 21:53:20 -0600

    did model comparison via anova.

--- multivariate.R
+++ multivariate.R
@@ -3,9 +3,20 @@ source("read_data.R")
 
 X_variables = DATA[,c(categorical[3], dummy[4], dummy[7])]
 
-glm_res = glm(Y_variables~X_variables[,1] + X_variables[,2] + X_variables[,3])
+glm_res1 = glm(Y_variables~X_variables[,1] + X_variables[,2] + X_variables[,3])
+summary(glm_res1)
 
-summary(glm_res)
+glm_res2 = glm(Y_variables~X_variables[,2] + X_variables[,3])
+summary(glm_res2)
+
+anova_result <- anova(glm_res1, glm_res2, test = "Chi") 
+anova_result
+
+# anova_result <- anova(glm_res2, glm_res1, test = "Chi") # reverse gives us the same result.
+# anova_result
+
+
+
 
 
 
